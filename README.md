a6ca781a4a9df064852fade0d7fb8216c821587b
Tim Marsh <tim@positivestudio.co.uk>
2016-12-18 08:18:24 +0000

    adding in my vim setup

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ my default bits and pieces for setting up / using a raspberry pi
 * script to get external ip address for stuff
 * install.sh to load the usual software
   * vnc
+  * htop
+  * jq
+  * screen
+* vimrc and script to load plugins
---
+++ vim/plugins.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+#nerdtree
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git
+
+#airline
+cd ~/.vim/bundle
+git clone https://github.com/bling/vim-airline
+
+#solarized
+cd ~/.vim/bundle
+git clone https://github.com/altercation/vim-colors-solarized
+
+#editorconfig
+cd ~/.vim/bundle
+git clone https://github.com/editorconfig/editorconfig-vim
+
+#fugitive
+cd ~/.vim/bundle
+git clone git://github.com/tpope/vim-fugitive.git
+vim -u NONE -c "helptags vim-fugitive/doc" -c q
